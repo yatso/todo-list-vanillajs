144c5ae481880743ef06acb82b7347f3e8c42865
yatso <yatso@Yats-MacBook-Pro-2.local>
2017-05-15 16:08:17 -0700

    Added comments to the displayTodos method
    So we don’t have to waste time figuring out what it does the next time
    we look at it.

--- js/app.js
+++ js/app.js
@@ -72,22 +72,32 @@ var handlers = {
 
 var view = {
   displayTodos: function() {
+    // grabs the <ul> element and sets it to todosUl variable.
     var todosUl = document.querySelector('ul');
+    // Clears out the content inside <ul> to make sure we're starting clean.
     todosUl.innerHTML = '';
     
+    // Note that the 'this' argument in the forEach method is to bind 'this' for the callback function so it has access to the view object inside the callback.
     todoList.todos.forEach(function(todo, position) {
+      // Creates an li element
       var todoLi = document.createElement('li');
+      // Initialize todoTextWithCompletion to empty string.
       var todoTextWithCompletion = '';
-
+      
+      // Builds the artificial checkboxes and the actual todo item text.
       if (todo.completed === true) {
         todoTextWithCompletion = '(x) ' + todo.todoText;
       } else {
         todoTextWithCompletion = '( ) ' + todo.todoText;
       }
       
+      // Sets the position of the forEach loop as the id for the todoLi element we're building.
       todoLi.id = position;
+      // Sets the artificial checkboxes and the actual todo item text to the inside of the created <li> element." <li>todoLi.textContent</li>"
       todoLi.textContent = todoTextWithCompletion;
+      // Adds the delete button as a child to the created <li> element by running the createDeleteButton method.
       todoLi.appendChild(this.createDeleteButton());
+      // Adds the finalized <li> element as a child to the <ul> element.
       todosUl.appendChild(todoLi);
     }, this);
   },
