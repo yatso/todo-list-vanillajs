15093922795501be20ccc8be2bc6f247a99cd1e2
sechun86 <chunjohn0@gmail.com>
2017-05-22 17:06:38 -0700

    v1.8
    HTML button changes
    Requirement 9 & 11

--- js/app.js
+++ js/app.js
@@ -171,7 +171,7 @@ var view = {
   },
   createDeleteButton: function() {
     var deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
+    deleteButton.textContent = 'X';
     deleteButton.className = 'deleteButton';
     return deleteButton;
   },
