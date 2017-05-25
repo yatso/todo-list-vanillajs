aef50cb80a218b3a9d3b582f8879eb0ebfb00efd
yatso <yatso@Yats-MacBook-Pro-2.local>
2017-05-24 21:18:24 -0700

    v2.2 Update box trigger button on mobile & minor space fixes
    -removed some unnecessary spacing
    -Removed some unnecessary return statements
    -Added some classes to html elements

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 //  Global variables declared for enter and escape keys.
 var enter_key = 13;
 var esc_key = 27;
-
 //  todoModel object represents the model of this app.
 var todoModel = {
   todos: [],
@@ -78,13 +77,11 @@ var controller = {
     var newUpdateInputValue = updateInputElement.value;
     if (updateInputElement.value && event.keyCode === enter_key) {
       this.changeTodo(id, newUpdateInputValue);
-      return;
     }
     if (event.keyCode === esc_key) {
       //  Very important line. If the updateInputElement.value is not reset to the original value, then updateFocusOut method would still run and update the data even when esc key is pressed.
       updateInputElement.value = todoModel.todos[id].todoText;
       view.displayTodos();
-      return;
     }
   },
   updateFocusOut: function(updateInputElement) {
@@ -92,7 +89,6 @@ var controller = {
     var newUpdateInputValue = updateInputElement.value;
     if (updateInputElement.value) {
       this.changeTodo(id, newUpdateInputValue);
-      return;
     } else {
       controller.deleteTodo(id);
     }
@@ -120,7 +116,6 @@ var controller = {
   toggleCompleted: function(toggleElement) {
     //  Gets the id from the parent (<li>) of the toggle check box and passes it to the toggleCompleted in the model. toggleCompleted switches the boolean value in the actual todo item object.
     todoModel.toggleCompleted(toggleElement.parentNode.getAttribute('id'));
-  
     //  Runs the view.displayTodos() to update the DOM so the user can see the changes.
     view.displayTodos();
   },
@@ -137,17 +132,14 @@ var view = {
     var todosUl = document.querySelector('ul');
     //  Clears out the content inside <ul> to make sure we're starting clean.
     todosUl.innerHTML = '';
-    
     //  Note that the 'this' argument in the forEach method is to bind 'this' for the callback function so it has access to the view object inside the callback.
     todoModel.todos.forEach(function(todo, position) {
       //  Creates an li element
       var todoLi = document.createElement('li');
-      
       //  Builds the checkbox
       var toggleCheckbox = document.createElement('input');
       toggleCheckbox.type = 'checkbox';
       toggleCheckbox.setAttribute('onchange', 'controller.toggleCompleted(this)');
-      
       //  Builds the updateBox
       var updateBox = document.createElement('input');
       updateBox.classList.add('updateBox','hide');
@@ -155,41 +147,34 @@ var view = {
       updateBox.value = todo.todoText;
       updateBox.setAttribute('onkeyup', 'controller.updateKeyup(this)');
       updateBox.setAttribute('onfocusout', 'controller.updateFocusOut(this)');
-      
       //  Builds the todo item text label
       var todoItemLabel = document.createElement('label');
       todoItemLabel.setAttribute('ondblclick', 'controller.updatingMode(this)');
       todoItemLabel.textContent = todo.todoText;
       todoItemLabel.classList.add('todoLabel');
-      
       //  Builds the mobile edit button
       var mobileEditButton = document.createElement('button');
       mobileEditButton.setAttribute('onclick', 'controller.mobileUpdatingMode(this)');
       mobileEditButton.textContent = 'Edit';
       mobileEditButton.classList.add('hide', 'editButton');
-      
       //  Removes hide class on mobile edit button if mobile is detected as true
       if (todoModel.isMobileDevice()) {
         mobileEditButton.classList.remove('hide');
       }
-      
       //  Toggles the strikethrough class when user clicks checkbox or toggle all.
       if (todo.completed === true) {
         todoItemLabel.classList.add('todos-strikethrough');
       } else {
         todoItemLabel.classList.remove('todos-strikethrough');
       }
-      
       //  Sets the position of the forEach loop as the id for the todoLi element we're building.
       todoLi.id = position;
-      
       //  Displays checked or unchecked for checkbox depending on todo item completed status.
       if (todo.completed === true) {
         toggleCheckbox.checked = true;
       } else {
         toggleCheckbox.checked = false;
       }
-      
       //  Adds the built-up toggleCheckbox as a child of the <li> element.
       todoLi.appendChild(toggleCheckbox);
       //  Adds the built-up todo item label as a child of the <li> element.
